63e2d9f7aa0a8b1aa3b11949809e887f274ddea1
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-09-05 12:47:04 +1000

    fix: favourite widget list partial

--- system/modules/favorite/partials/actions/listfavorite.php
+++ system/modules/favorite/partials/actions/listfavorite.php
@@ -1,27 +1,41 @@
 <?php
+
 /**
  * Partial action that lists favorite objects
  * @author Steve Ryan steve@2pisoftware.com 2015
  */
-function listfavorite_ALL(Web $w,$params) {
+function listfavorite_ALL(Web $w, $params)
+{
 	$user = AuthService::getInstance($w)->user();
-	if (!empty($user))  {
-		$results = FavoriteService::getInstance($w)->getFavoritesForUser($user->id );
-		$favoritesCategorised=array();
-		$service=new DBService($w);
-		if (!empty($results)) {
-			foreach ($results as $k => $favorite) {
-				if (!array_key_exists($favorite->object_class,$favoritesCategorised)) $favoritesCategorised[$favorite->object_class]=array();
-				$realObject=$service->getObject($favorite->object_class,$favorite->object_id);
-				if (!empty($realObject))  {
-					$templateData=array();
-					$templateData['title']=$realObject->printSearchTitle();
-					$templateData['url']=$realObject->printSearchUrl();
-					$templateData['listing']=$realObject->printSearchListing();
-					if ($realObject->canList($user) && $realObject->canView($user)) array_push($favoritesCategorised[$favorite->object_class],$templateData);
-				}
+
+	if (empty($user)) {
+		return;
+	}
+
+	$results = FavoriteService::getInstance($w)->getFavoritesForUser($user->id);
+	$favoritesCategorised = [];
+	$service = new DBService($w);
+
+	if (empty($results)) {
+		return;
+	}
+
+	foreach ($results as $k => $favorite) {
+		if (!array_key_exists($favorite->object_class, $favoritesCategorised)) {
+			$favoritesCategorised[$favorite->object_class] = [];
+		}
+
+		$realObject = $service->getObject($favorite->object_class, $favorite->object_id);
+		if (!empty($realObject)) {
+			$templateData = [];
+			$templateData['title'] = $realObject->printSearchTitle();
+			$templateData['url'] = $realObject->printSearchUrl();
+			$templateData['listing'] = $realObject->printSearchListing();
+			$templateData["ref"] = $realObject;
+			if ($realObject->canList($user) && $realObject->canView($user)) {
+				array_push($favoritesCategorised[$favorite->object_class], $templateData);
 			}
 		}
-		$w->ctx('categorisedFavorites',$favoritesCategorised);
 	}
+	$w->ctx('categorisedFavorites', $favoritesCategorised);
 }
