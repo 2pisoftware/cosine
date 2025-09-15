476a733e5159d3f033064d5e5d4f65ccd2609c1a
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-09-15 12:26:31 +1000

    chore: lint

--- system/modules/main/partials/actions/simple_pagination.php
+++ system/modules/main/partials/actions/simple_pagination.php
@@ -2,9 +2,9 @@
 
 function simple_pagination(Web $w, array $params)
 {
-	$w->ctx("current", $params["current"]);
-	$w->ctx("page_size", $params["page_size"]);
-	$w->ctx("total", $params["total"]);
-	$w->ctx("param", $params["param"]);
-	$w->ctx("base_url", $params["base_url"]);
+    $w->ctx("current", $params["current"]);
+    $w->ctx("page_size", $params["page_size"]);
+    $w->ctx("total", $params["total"]);
+    $w->ctx("param", $params["param"]);
+    $w->ctx("base_url", $params["base_url"]);
 }
--- system/modules/main/partials/templates/simple_pagination.tpl.php
+++ system/modules/main/partials/templates/simple_pagination.tpl.php
@@ -2,64 +2,64 @@
 
 function makeUrl(int $page, string $base_url, string $param)
 {
-	$parsed = parse_url($base_url);
+    $parsed = parse_url($base_url);
 
-	return $parsed["path"] . "?" . $parsed["query"] . "&" . http_build_query([
-		$param => $page
-	]);
+    return $parsed["path"] . "?" . $parsed["query"] . "&" . http_build_query([
+        $param => $page
+    ]);
 }
 
 $last = ceil($total / $page_size);
 ?>
 
 <nav aria-label="pagination">
-	<ul class="pagination justify-content-center flex-wrap">
-		<?php if ($current > 1) : ?>
-			<li class="page-item">
-				<a
-					class="page-link"
-					href="<?php echo makeUrl(1, $base_url, $param) ?>">
-					1
-				</a>
-			</li>
-		<?php endif; ?>
+    <ul class="pagination justify-content-center flex-wrap">
+        <?php if ($current > 1) : ?>
+            <li class="page-item">
+                <a
+                    class="page-link"
+                    href="<?php echo makeUrl(1, $base_url, $param) ?>">
+                    1
+                </a>
+            </li>
+        <?php endif; ?>
 
-		<?php if ($current > 2) : ?>
-			<li class="page-item">
-				<a
-					class="page-link"
-					href="<?php echo makeUrl($current - 1, $base_url, $param) ?>">
-					<?php echo $current - 1 ?>
-				</a>
-			</li>
-		<?php endif; ?>
+        <?php if ($current > 2) : ?>
+            <li class="page-item">
+                <a
+                    class="page-link"
+                    href="<?php echo makeUrl($current - 1, $base_url, $param) ?>">
+                    <?php echo $current - 1 ?>
+                </a>
+            </li>
+        <?php endif; ?>
 
-		<li class="page-item disabled">
-			<a
-				class="page-link"
-				href="<?php echo makeUrl($current, $base_url, $param) ?>">
-				<?php echo $current; ?>
-			</a>
-		</li>
+        <li class="page-item disabled">
+            <a
+                class="page-link"
+                href="<?php echo makeUrl($current, $base_url, $param) ?>">
+                <?php echo $current; ?>
+            </a>
+        </li>
 
-		<?php if ($current < $last - 1) : ?>
-			<li class="page-item">
-				<a
-					class="page-link"
-					href="<?php echo makeUrl($current + 1, $base_url, $param) ?>">
-					<?php echo $current + 1; ?>
-				</a>
-			</li>
-		<?php endif; ?>
+        <?php if ($current < $last - 1) : ?>
+            <li class="page-item">
+                <a
+                    class="page-link"
+                    href="<?php echo makeUrl($current + 1, $base_url, $param) ?>">
+                    <?php echo $current + 1; ?>
+                </a>
+            </li>
+        <?php endif; ?>
 
-		<?php if ($current < $last) : ?>
-			<li class="page-item">
-				<a
-					class="page-link"
-					href="<?php echo makeUrl($last, $base_url, $param) ?>">
-					<?php echo $last; ?>
-				</a>
-			</li>
-		<?php endif; ?>
-	</ul>
+        <?php if ($current < $last) : ?>
+            <li class="page-item">
+                <a
+                    class="page-link"
+                    href="<?php echo makeUrl($last, $base_url, $param) ?>">
+                    <?php echo $last; ?>
+                </a>
+            </li>
+        <?php endif; ?>
+    </ul>
 </nav>
