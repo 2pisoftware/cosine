ae3faf6e1e7dd21f4c0ad3f7ad29788767f3d56a
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-01-16 13:42:10 +1100

    fix: prevent timelog action buttons from wrapping

--- system/modules/timelog/templates/index.tpl.php
+++ system/modules/timelog/templates/index.tpl.php
@@ -8,35 +8,35 @@
                 </div>
             </h4>
             <?php
-                $header = ["From", "To", "Object", "Description", "Actions"];
-                echo HtmlBootstrap5::table(array_map(function ($val) use ($w) {
-                    $row = [
-                        formatDate($val->dt_start, "H:i:s"),
-                        formatDate($val->dt_end, "H:i:s"),
-                        class_exists($val->object_class) ? ($val->getLinkedObject() ? get_class(object: $val->getLinkedObject()) . ": " . $val->getLinkedObject()->toLink() : '') : 'Invalid Timelog object',
-                        "<pre class='break-pre text-truncate d-block m-0' style='width: 250px;''>" . strip_tags($val->getComment()->comment) . "</pre>",
-                    ];
+            $header = ["From", "To", "Object", "Description", "Actions"];
+            echo HtmlBootstrap5::table(array_map(function ($val) use ($w) {
+                $row = [
+                    formatDate($val->dt_start, "H:i:s"),
+                    formatDate($val->dt_end, "H:i:s"),
+                    class_exists($val->object_class) ? ($val->getLinkedObject() ? get_class(object: $val->getLinkedObject()) . ": " . $val->getLinkedObject()->toLink() : '') : 'Invalid Timelog object',
+                    "<pre class='break-pre text-truncate d-block m-0' style='width: 250px;''>" . strip_tags($val->getComment()->comment) . "</pre>",
+                ];
 
-                    $actions = [];
+                $actions = [];
 
-                    if ($val->object_class == "Task") {
-                        $actions[] = HtmlBootstrap5::b('/task/edit/' . $val->object_id . "#timelog", "View Time Log", null, null, null, "btn btn-sm btn-primary");
-                    }
+                if ($val->object_class == "Task") {
+                    $actions[] = HtmlBootstrap5::b('/task/edit/' . $val->object_id . "#timelog", "View", null, null, null, "btn btn-sm btn-primary");
+                }
 
-                    if ($val->canEdit(AuthService::getInstance($w)->user())) {
-                        $actions[] = HtmlBootstrap5::box('/timelog/edit/' . $val->id, 'Edit', true, null, null, null, "isbox", null, "btn btn-sm btn-primary");
-                        $actions[] = HtmlBootstrap5::box('/timelog/move/' . $val->id, 'Move', true, null, null, null, "isbox", null, "btn btn-sm btn-primary");
-                    }
+                if ($val->canEdit(AuthService::getInstance($w)->user())) {
+                    $actions[] = HtmlBootstrap5::box('/timelog/edit/' . $val->id, 'Edit', true, null, null, null, "isbox", null, "btn btn-sm btn-primary");
+                    $actions[] = HtmlBootstrap5::box('/timelog/move/' . $val->id, 'Move', true, null, null, null, "isbox", null, "btn btn-sm btn-primary");
+                }
 
-                    if ($val->canDelete(AuthService::getInstance($w)->user())) {
-                        $confirmation_message = implode("", $w->callHook("timelog", "before_display_timelog", $val));
-                        $actions[] = HtmlBootstrap5::b('/timelog/delete/' . $val->id, 'Delete', empty($confirmation_message) ? 'Are you sure you want to delete this timelog?' : $confirmation_message, null, null, "btn btn-sm btn-danger");
-                    }
+                if ($val->canDelete(AuthService::getInstance($w)->user())) {
+                    $confirmation_message = implode("", $w->callHook("timelog", "before_display_timelog", $val));
+                    $actions[] = HtmlBootstrap5::b('/timelog/delete/' . $val->id, 'Delete', empty($confirmation_message) ? 'Are you sure you want to delete this timelog?' : $confirmation_message, null, null, "btn btn-sm btn-danger");
+                }
 
-                    $row[] = HtmlBootstrap5::buttonGroup(implode("", $actions));
+                $row[] = '<div class="text-nowrap">' . HtmlBootstrap5::buttonGroup(implode("", $actions)) . '</div>';
 
-                    return $row;
-                }, $entry_struct["entries"]), null, "tablesorter", $header);
+                return $row;
+            }, $entry_struct["entries"]), null, "tablesorter", $header);
             ?>
         <?php endforeach; ?>
     <?php else : ?>
