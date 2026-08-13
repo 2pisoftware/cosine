cf874c097b9602501073fcf665af7c9a72286ac5
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-08-13 13:17:47 +1000

    fix: ignore warnings in ./lint.sh

--- lint.sh
+++ lint.sh
@@ -2,4 +2,4 @@
 
 phpcbf --standard=phpcs.xml --parallel=$(nproc) --ignore="*.js,*.css,system/lib/*" system
 
-phpcs --standard=phpcs.xml --parallel=$(nproc) --ignore="*.js,*.css,system/lib/*" system
+phpcs --config-set ignore_warnings_on_exit 1 --standard=phpcs.xml --parallel=$(nproc) --ignore="*.js,*.css,system/lib/*" system
